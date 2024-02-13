5b585ebf3afdfb5107ea47f96b77951d73296a7e
DEVADHARSHINI A <128233650+DEVADHARSHINIAKSHAYA@users.noreply.github.com>
2024-02-13 15:53:04 +0530

    Add files via upload

---
+++ schema.cjs
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const restaurantsSchema = new mongoose.Schema({
+    areaName: {
+        type: String
+    },
+    avgRating: {
+        type: Number
+    },
+    costForTwo: {
+        type: String
+    },
+    cuisines: {
+        type: Array
+    },
+    name: {
+        type: String
+    }
+})
+const Restaurant = mongoose.model('restaurantdetail', restaurantsSchema)
+
+const userSchema = new mongoose.Schema({
+    contact: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true,
+
+    }
+
+
+}, { versionKey: false })
+
+// model
+const Users = mongoose.model('userDetail', userSchema)
+
+// Exporting the model
+module.exports = { Users, Restaurant }
---
+++ server.cjs
@@ -0,0 +1,107 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const { Restaurant, Users } = require('./schema.cjs')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+async function connectToDb() {
+    try {
+        await mongoose.connect('mongodb+srv://DEVADHARSHINI:22022003@cluster0.5uzsg9b.mongodb.net/Swiggy?retryWrites=true&w=majority')
+        console.log(`DB Connection Established`)
+        const port = process.env.port || 8000
+        app.listen(port, function() {
+            console.log(`Listening on port ${port}...........`)
+        })
+    } catch (error) {
+        console.log(error)
+        console.log('Couldn\'t Establish connection :(')
+
+    }
+}
+connectToDb()
+app.post('/add-restaurant', async function(request, response) {
+    console.log(request.body)
+    try {
+        await Restaurant.create({
+            "areaName": request.body.areaName,
+            "avgRating": request.body.avgRating,
+            "costForTwo": request.body.costForTwo,
+            "cuisines": request.body.cuisines,
+            "name": request.body.name
+        })
+        response.status(201).json({
+            "status": "success",
+            "message": "user created"
+        })
+    } catch (error) {
+        response.status(500).json({
+            "status": "failure",
+            "message": "internal server error",
+            "error": error
+        })
+    }
+})
+
+app.get('/get-restaurant-details', async function(request, response) {
+    try {
+        const restaurantDetails = await Restaurant.find()
+        response.status(200).json(restaurantDetails)
+
+    } catch (error) {
+        response.status(500).json({
+            "status": "failure",
+            "message": "couldnot fetch details",
+            "error": error
+        })
+    }
+})
+
+app.post('/create-new-user', async function(request, response) {
+    try {
+        await Users.create({
+            "userName": request.body.username,
+            "email": request.body.email,
+            "password": request.body.password,
+            "contact": request.body.contact
+        })
+        response.status(201).json({
+            "status": "success",
+            "message": "User created"
+        })
+    } catch (error) {
+        response.status(500).json({
+            "status": "failure",
+            "message": "Internal server error",
+            "error": error
+        })
+    }
+})
+app.post('/validate-user', async function(request, response) {
+    try {
+        const user = await Users.findOne({
+            "email": request.body.email,
+            "password": request.body.password
+        })
+        if (user) {
+            response.status(200).json({
+                "messsage": "valid user"
+            })
+
+        } else {
+            response.status(401).json({
+                "message": "invalid user",
+                "error": error
+            })
+        }
+
+    } catch (error) {
+        response.status(500).json({
+            "message": "Internal server error"
+        })
+    }
+
+})
